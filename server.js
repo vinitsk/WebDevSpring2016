1922187966bf324a39957893a1c0c833eb7ed42f
Bhanu Jain <bhanujain88@gmail.com>
2016-01-19 01:21:53 -0500

    Adding Public Folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.use(express.static(__dirname + '/public'));
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
