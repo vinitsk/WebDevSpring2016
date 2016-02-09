49b9848b4b5bb7e3578d4e418842cc41045fc297
Bhanu Jain <bhanujain88@gmail.com>
2016-02-09 00:20:40 -0500

    Adding OMDB API

--- public/experiments/omdbapi/js/app.js
+++ public/experiments/omdbapi/js/app.js
@@ -5,17 +5,29 @@
     var $movieTitleTxt;
     var $searchBtn;
     var $searchResults;
+    var $plot;
+    var $actors;
+    var $title;
+    var $poster;
+    var $director;
     var SEARCH_URL = "http://www.omdbapi.com/?s=TITLE";
+    var DETAILS_URL = "http://www.omdbapi.com/?i=IMDBID";
 
 
     function init() {
         $movieTitleTxt = $("#movieTitleTxt");
         $searchBtn = $("#searchBtn");
         $searchResults = $("#searchResults tbody")
+        $plot = $("#plot");
+        $actors = $("#actors");
+        $title = $("#title");
+        $poster = $("#poster");
+        $director = $("#director");
         $searchBtn.click(searchMovie);
     }
 
     function searchMovie() {
+        $searchResults.empty();
         var title = $movieTitleTxt.val();
         var url = SEARCH_URL.replace("TITLE", title);
 
@@ -26,7 +38,37 @@
     }
 
     function fetchMovieDetails(event) {
-        console.log(event);
+        var $tr = $(event.currentTarget);
+        var imdbID = $tr.attr("id");
+
+        var url = DETAILS_URL.replace("IMDBID", imdbID);
+        $.ajax({
+            url: url,
+            success: renderMovieDetails
+        });
+    }
+
+    function renderMovieDetails(response) {
+        console.log(response);
+
+        var actors = response.Actors;
+        var title = response.Title;
+        var director = response.Director;
+        var plot = response.Plot;
+        var posterUrl = response.Poster;
+
+        $actors.empty();
+        var actorArray = actors.split(",");
+        for (var a in actorArray) {
+            var actor = actorArray[a];
+            var $li = $("<li>").append(actor).appendTo($actors);
+        }
+        $plot.html(plot);
+        $title.html(title);
+        $director.html(director);
+        $poster.attr("src", posterUrl);
+
+
     }
 
     function renderSearchResults(response) {
@@ -44,7 +86,7 @@
 
             var $img = $("<img>")
                 .attr("src", posterUrl)
-                    .addClass("posterThumb");
+                .addClass("posterThumb");
 
             var $tr = $("<tr>")
                 .attr("id", imdbID)
