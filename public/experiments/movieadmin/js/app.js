22c6169ef8290b4db8e54a5e6d9af83e825ddbbc
Bhanu Jain <bhanujain88@gmail.com>
2016-02-12 11:31:20 -0500

    Adding Angular Exp

--- public/experiments/movieadmin/js/app.js
+++ public/experiments/movieadmin/js/app.js
@@ -1,18 +1,24 @@
-(function(){
+(function () {
 
     angular
-        .module("MovieAdminApp",[])
-        .controller("MovieController",MovieController);
+        .module("MovieAdminApp", [])
+        .controller("MovieController", MovieController);
 
-    function MovieController($scope){
+    function MovieController($scope) {
         $scope.hello = "Hello from the controller."
         $scope.movies = [
-            {id: 123, title: "Star Wars I", director:"JJ A"},
-            {id: 134, title: "Star Wars II", director:"JJ A"},
-            {id: 145, title: "Star Wars III", director:"JJ A"}
+            {id: 123, title: "Star Wars I", director: "JJ A"},
+            {id: 134, title: "Star Wars II", director: "JJ A"},
+            {id: 145, title: "Star Wars III", director: "JJ A"}
         ]
 
-        $scope.addMovie = function(){
+
+        $scope.addMovie = addMovie;
+        $scope.deleteMovie = deleteMovie;
+        $scope.selectMovie = selectMovie;
+        $scope.updateMovie = updateMovie;
+
+        function addMovie() {
             var newMovie = {
                 id: $scope.movie.id,
                 title: $scope.movie.title,
@@ -22,9 +28,24 @@
             $scope.movie = {};
         }
 
-        $scope.deleteMovie = function (index) {
-            $scope.movies.splice(index,1);
+        function deleteMovie(movie) {
+            var index = $scope.movies.indexOf(index);
+            $scope.movies.splice(index, 1);
         }
+
+        function selectMovie(movie) {
+            $scope.movie = {
+                id: movie.id,
+                title: movie.title,
+                director: movie.director
+            };
+            $scope.selMovieIndex = $scope.movies.indexOf(movie);
+        }
+
+        function updateMovie(){
+
+        }
+
     };
 
 })();
