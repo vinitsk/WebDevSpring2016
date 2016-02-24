f7288278aa10af9e34ceaa5a68b275e9daba494b
Bhanu Jain <bhanujain88@gmail.com>
2016-02-24 02:15:01 -0500

    Updating Work

--- public/profile/js/init.js
+++ public/profile/js/init.js
@@ -9,9 +9,11 @@
       if ($(document).scrollTop() > 100) {
         $('#mainNav').removeClass('top-transparent');
         $('#mainNav').addClass('top-color');
+        $('#menu-button').removeClass('grey-text text-darken-1');
       } else {
         $('#mainNav').removeClass('top-color');
         $('#mainNav').addClass('top-transparent');
+        $('#menu-button').addClass('grey-text text-darken-1');
       }
     });
 
