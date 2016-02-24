d222ec17aa4feb808706e28faeeba4b13f98c44e
Bhanu Jain <bhanujain88@gmail.com>
2016-02-24 02:06:16 -0500

    Updating Work

--- public/profile/js/init.js
+++ public/profile/js/init.js
@@ -4,5 +4,16 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 100) {
+        $('#mainNav').removeClass('top-transparent');
+        $('#mainNav').addClass('top-color');
+      } else {
+        $('#mainNav').removeClass('top-color');
+        $('#mainNav').addClass('top-transparent');
+      }
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
