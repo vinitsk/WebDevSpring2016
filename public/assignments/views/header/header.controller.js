41d0a9d2709d6049844c95a37a5beca8e72f42d2
Bhanu Jain <bhanujain88@gmail.com>
2016-03-03 14:35:59 -0500

    Assignment 2 - Updating Header and Sidebar

--- public/assignments/views/header/header.controller.js
+++ public/assignments/views/header/header.controller.js
@@ -7,9 +7,17 @@
         .module("FormBuilderApp")
         .controller("HeaderController", HeaderController)
 
-    function HeaderController($location, $scope) {
+    function HeaderController($location, $scope, $rootScope) {
 
         $scope.$location = $location;
 
+        //Event Handler Deceleration
+        $scope.logout = logout;
+
+        //Event Handler Implementation
+        function logout() {
+            delete $rootScope.user;
+            $location.url("/home");
+        }
     };
 })();
