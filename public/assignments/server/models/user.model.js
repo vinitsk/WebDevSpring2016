5135b768a8ee78cfd5210ffb83ff31000c9610f0
Bhanu Jain <bhanujain88@gmail.com>
2016-03-18 00:31:21 -0400

    Assignment 03

---
+++ public/assignments/server/models/user.model.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Bhanu on 18/03/2016.
+ */
+
+"use strict";
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+var bodyParser    = require('body-parser');
+var multer        = require('multer');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 app.get('/hello', function(req, res){
     res.send('hello world');
