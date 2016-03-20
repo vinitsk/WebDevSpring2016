5061884750c941acc081f3de43778b1f81bbc739
Bhanu Jain <bhanujain88@gmail.com>
2016-03-19 21:10:05 -0400

    Assignment 03 - Fixing Node Startup Error

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ var multer        = require('multer');
 var cookieParser  = require('cookie-parser');
 var session       = require('express-session');
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
 app.use(express.static(__dirname + '/public'));
 
-require("./public/assignments/server/app.js")(app);
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
+
+require("./public/assignments/server/app.js")(app);
 app.listen(port, ipaddress);
