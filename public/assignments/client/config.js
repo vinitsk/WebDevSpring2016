bd3b368c81e9623fa65a9c0e13f09de8ecf083a1
Bhanu Jain <bhanujain88@gmail.com>
2016-04-22 18:47:10 -0400

    Passport

--- public/assignments/client/config.js
+++ public/assignments/client/config.js
@@ -13,13 +13,18 @@
         $routeProvider
             .when("/home", {
                 templateUrl: "views/home/home.view.html",
-                controller: "HomeController"
+                controller: "HomeController",
+                resolve: {
+                    loggedin: checkCurrentUser
+                }
             })
             .when("/:userId/admin", {
                 templateUrl: "views/admin/admin.view.html",
                 controller: "AdminController",
                 controllerAs: "model",
-                resolve: {loggedin: checkLoggedin}
+                resolve: {
+                    loggedin: checkAdmin
+                }
             })
             .when("/:userId/profile", {
                 templateUrl: "views/users/profile.view.html",
@@ -73,6 +78,52 @@
                 $location.url('/');
             }
         });
+        return deferred.promise;
+    };
+
+    var checkAdmin = function($q, $timeout, $http, $location, $rootScope)
+    {
+        var deferred = $q.defer();
+
+        $http.get('/api/assignment/loggedin').success(function(user)
+        {
+            $rootScope.errorMessage = null;
+            // User is Authenticated
+            if (user !== '0' && user.roles.indexOf('admin') > -1)
+            {
+                $rootScope.user = user;
+                deferred.resolve();
+            }
+            else if(user=='0')
+            {
+                //$rootScope.errorMessage = 'You need to log in.';
+                alert("You need to log in.");
+                deferred.reject();
+                $location.url('/login');
+                //$rootScope.danger = "Unable to login";
+            }
+            else{
+                $location.url('/home');
+            }
+
+        });
+
+        return deferred.promise;
+    };
+    var checkCurrentUser = function($q, $timeout, $http, $location, $rootScope)
+    {
+        var deferred = $q.defer();
+
+        $http.get('/api/assignment/loggedin').success(function(user)
+        {
+            // User is Authenticated
+            if (user !== '0')
+            {
+                $rootScope.user = user;
+            }
+            deferred.resolve();
+        });
+
         return deferred.promise;
     };
 })();
